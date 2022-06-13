6f90480d734a4fd6a773f38d38f48a0aeee6563b
Rayshawn <rayshawnbray@gmail.com>
2022-06-13 16:21:36 -0400

    Make 16 x 16 grid appear on DOM and add hover effect over each cell

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log('hello world');
+
+// bring in the container
+const gridContainer = document.getElementById('grid');
+
+// make function to generate rows and columns
+function makeRows(rows, cols) {
+    // put the grid properties on the rows and columns
+    gridContainer.style.setProperty('--grid-rows', rows);
+    gridContainer.style.setProperty('--grid-cols', cols);
+
+    // loop to make boxes appear on DOM
+    for (let c = 0; c < (rows * cols); c++) {
+        // create each cell
+        let cell = document.createElement('div');
+        // add classlist
+        cell.classList.add('grid-item');
+        // append to the DOM
+        gridContainer.appendChild(cell);
+    }
+}
+
+// call function
+makeRows(16, 16);
