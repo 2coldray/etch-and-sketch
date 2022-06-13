fa316f82a2fca4433b5224b84d34df8529a61a84
Rayshawn <rayshawnbray@gmail.com>
2022-06-13 18:28:15 -0400

    Modify the html file with headings and reset button as well as chnage background color of grids when they are hovered over

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ function makeRows(rows, cols) {
         let cell = document.createElement('div');
         // add classlist
         cell.classList.add('grid-item');
+        // add id to cell
+        cell.setAttribute('id', c + 1);
         // append to the DOM
         gridContainer.appendChild(cell);
     }
@@ -22,3 +24,18 @@ function makeRows(rows, cols) {
 
 // call function
 makeRows(16, 16);
+
+// save grid items in a node list
+const gridItems = document.querySelectorAll('.grid-item');
+
+// loop over list
+gridItems.forEach(item => {
+    // add a mouse enter event to the to each square 
+    item.addEventListener('mouseenter', function (e) {
+        // change the background color to orange 
+        e.target.classList.add('orange');
+    })
+})
+
+// remove the class
+const resetBtn = document.getElementById('reset');
